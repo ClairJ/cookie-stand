e5efc888abfbf8c7d063f0157c4e5b7e09b367fe
Joel Clair <clair.joel@yahoo.com>
2017-09-26 21:41:37 -0700

    allStores table is working

--- app.js
+++ app.js
@@ -1,39 +1,69 @@
 'use strict';
-var storeNames = ['1st and Pike', 'SeaTac Airport', 'Seattle Center', 'Capitol Hill', 'Alki'];
+//var stores = [];
 var openHrs = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
-var stores = [];
-var totals = [];
 
+var allStores = [];
+var storeTable = document.getElementById('stores');
 function Store(name,min,max,avgCS) {
-  this.hourlyCookiesArr = [];
   this.minCust = min;
   this.maxCust = max;
   this.avgCS = avgCS;
   this.name = name;
+  this.avgCustPH = [];
+  this.avgCookPH = [];
+  allStores.push(this);
 }
-var pike = new Store('pike','23','65','6.3');
-Store.prototype.avgCust = function() {
-  var avgCustomer = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-  return avgCustomer;
+Store.prototype.render = function(){
+  var trEl = docuemtn.createElement('tr');
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.name;
+  trEl.appendChild(tdEl);
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.avgCookPH;
+  trEl.appendChild(tdEl);
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = openHrs;
+  trEl.appendChild(tdEl);
+
+  storeTable.appendChild(trEl);
 };
-pike.avgCust();
-// this.hourlySales = function() {
+
+
+Store.prototype.avgCust = function() {
+  for (var i = 0; i < openHrs.length; i++) {
+    this.avgCustPH.push(Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust));
+  }
+};
+
+//stores hourly cookies sold
+Store.prototype.avgCPH = function() {
+  for (var i = 0; i < openHrs.length; i++) {
+    this.avgCookPH.push(Math.round(this.avgCustPH[i] * this.avgCS));
+  }
+};
+
+// Store.prototype.totalsMath = function() {
 //   for (var i = 0; i < openHrs.length; i++) {
-//     var cookiesSold = Math.round(this.avgCust() * this.avgCS);
-//     return cookiesSold;
-//     totals[i] += cookiesSold;
+//     var totals2 = this.avgCookPH[i] + i;
+//     this.total.push(totals2);
 //   }
 // };
-//   stores.push(this);
-// }
-// new Store('pike','23','65','6.3');
-//
-// var pike = new Store('pike','23','65','6.3');
-// console.log(pike);
-// console.log(avgCust);
-//
-// Store.prototype.avgCust = function() {
-//   console.log('avgCust: ' + avgCust);
-// };
-//
-// birdie.hello();
+var pike = new Store('pike','23','65','6.3');
+var seaTac = new Store('seaTac','3','24','1.2');
+var seaCenter = new Store('seaCenter','11','38','3.7');
+var capitolHill = new Store('capitolHill','20','38','2.3');
+var alki = new Store('alki','2','16','4.6');
+//stores avg customers
+console.table(allStores);
+pike.avgCust();
+pike.avgCPH();
+seaTac.avgCust();
+seaTac.avgCPH();
+seaCenter.avgCust();
+seaCenter.avgCPH();
+capitolHill.avgCust();
+capitolHill.avgCPH();
+alki.avgCust();
+alki.avgCPH();
